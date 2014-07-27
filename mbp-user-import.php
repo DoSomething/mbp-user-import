3f61495c7a2ff4761b07682f454434bcd64f6d1e
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-07-27 13:16:10 -0400

    First draft of producer

---
+++ mbp-user-import.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * mbp-user-import.php
+ *
+ * Import user data from CSV file supplied by niche.com of users interested in
+ * DoSomething scholarships. Entries in the userImportQueue will be consumed by
+ * mbc-user-import consumer. User creation in the Drupal website as well as the
+ * userAPI, Mailchimp and Mandrill transactional signup email message will be
+ * triggered by each entry.
+ */
+
+// Load up the Composer autoload magic
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Load configuration settings common to the Message Broker system
+// symlinks in the project directory point to the actual location of the files
+require __DIR__ . '/mb-secure-config.inc';
+require __DIR__ . '/mb-config.inc';
+
+require __DIR__ . '/MBP_userImport.class.inc';
+
+// Settings
+$credentials = array(
+  'host' =>  getenv("RABBITMQ_HOST"),
+  'port' => getenv("RABBITMQ_PORT"),
+  'username' => getenv("RABBITMQ_USERNAME"),
+  'password' => getenv("RABBITMQ_PASSWORD"),
+  'vhost' => getenv("RABBITMQ_VHOST"),
+);
+
+$config = array(
+  'exchange' => array(
+    'name' => getenv("MB_USER_IMPORT_EXCHANGE"),
+    'type' => getenv("MB_USER_IMPORT_EXCHANGE_TYPE"),
+    'passive' => getenv("MB_USER_IMPORT_EXCHANGE_PASSIVE"),
+    'durable' => getenv("MB_USER_IMPORT_EXCHANGE_DURABLE"),
+    'auto_delete' => getenv("MB_USER_IMPORT_EXCHANGE_AUTO_DELETE"),
+  ),
+  'queue' => array(
+    array(
+      'name' => getenv("MB_USER_IMPORT_QUEUE"),
+      'passive' => getenv("MB_USER_IMPORT_QUEUE_PASSIVE"),
+      'durable' => getenv("MB_USER_IMPORT_QUEUE_DURABLE"),
+      'exclusive' => getenv("MB_USER_IMPORT_QUEUE_EXCLUSIVE"),
+      'auto_delete' => getenv("MB_USER_IMPORT_QUEUE_AUTO_DELETE"),
+      'bindingKey' => getenv("MB_USER_IMPORT_QUEUE_BINDING_KEY"),
+    ),
+  ),
+  'routingKey' => getenv("MB_USER_IMPORT_ROUTING_KEY"),
+);
+$settings = array(
+  'stathat_ez_key' => getenv("STATHAT_EZKEY"),
+);
+
+$status = NULL;
+
+echo '------- mbp-user-import START: ' . date('D M j G:i:s T Y') . ' -------', "\n";
+
+// Kick off
+// Create entries in userImportQueue based on csv.
+$mbpUserImport = new MBP_UserEvent($credentials, $config, $settings);
+$mbpUserImport->produceCSVImport();
+
+echo '------- mbp-user-import END: ' . date('D M j G:i:s T Y') . ' -------', "\n";
