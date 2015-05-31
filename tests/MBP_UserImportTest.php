a52c161e8ecd22b06210dffd25b344e4f96f9af6
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-05-31 19:16:43 -0400

    Stubs out testFindNextTargetFile()

--- tests/MBP_UserImportTest.php
+++ tests/MBP_UserImportTest.php
@@ -3,12 +3,28 @@
 use DoSomething\MBP_UserImport\MBP_UserImport;
  
 class MBP_UserImportTest extends PHPUnit_Framework_TestCase {
+  
+  public function setUp(){ }
+  public function tearDown(){ }
  
   public function testFindNextTargetFile()
   {
+    
+    date_default_timezone_set('America/New_York');
 
-    require_once '../mbp-user-import.config.inc';
-    $mbpUserDigest = new MBP_UserImport($credentials, $settings, $config);
+    // Load up the Composer autoload magic
+    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../mbp-user-import.config.inc';
+    
+    echo 'credentials: ' . print_r($credentials), PHP_EOL;
+    echo 'settings: ' . print_r($settings), PHP_EOL;
+    echo 'config: ' . print_r($config), PHP_EOL;
+    
+//    $mbpUserImport = new MBP_UserImport($credentials, $settings, $config);
+    
+/*
+    
+//    echo 'mbpUserDigest: ' . print_r($mbpUserImport, TRUE), PHP_EOL;
     
     $sources = array(
       'niche',
@@ -16,6 +32,9 @@ class MBP_UserImportTest extends PHPUnit_Framework_TestCase {
       'hercampus',
       'teenlife',
     );
+    
+//    echo 'sources: ' . print_r($sources, TRUE), PHP_EOL;
+    
     foreach ($sources as $source) {
       $targetCSVFile = $mbpUserImport->findNextTargetFile($source);
       
@@ -23,6 +42,8 @@ class MBP_UserImportTest extends PHPUnit_Framework_TestCase {
       $foundCSVFile = TRUE;
       $this->assertTrue($foundCSVFile);
     }
+    
+*/
 
   }
  
