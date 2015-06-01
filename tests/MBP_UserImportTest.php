3acf603a28a2f62f3dcca9bcd82488b149e14460
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-05-31 20:11:44 -0400

    Supporting comments

--- tests/MBP_UserImportTest.php
+++ tests/MBP_UserImportTest.php
@@ -12,20 +12,27 @@ class MBP_UserImportTest extends PHPUnit_Framework_TestCase {
     
     date_default_timezone_set('America/New_York');
 
-    // Load up the Composer autoload magic
-    require_once __DIR__ . '/../vendor/autoload.php';
+    // Including that file will also return the autoloader instance, so you can store
+    // the return value of the include call in a variable and add more namespaces.
+    // This can be useful for autoloading classes in a test suite, for example.
+    // https://getcomposer.org/doc/01-basic-usage.md
+    $loader = require_once __DIR__ . '/../vendor/autoload.php';
+    
+    // Load Message Broker settings used mb mbp-user-import.php
     require_once __DIR__ . '/../mbp-user-import.config.inc';
     
     echo 'credentials: ' . print_r($credentials), PHP_EOL;
     echo 'settings: ' . print_r($settings), PHP_EOL;
     echo 'config: ' . print_r($config), PHP_EOL;
     
+    // Create  MBP_UserImport object to access findNextTargetFile() method for testing
 //    $mbpUserImport = new MBP_UserImport($credentials, $settings, $config);
     
 /*
-    
 //    echo 'mbpUserDigest: ' . print_r($mbpUserImport, TRUE), PHP_EOL;
     
+    // List of valid sources
+    // @todo: Move this to include file so production and app can share singe files as settings.
     $sources = array(
       'niche',
       'att-ichannel',
