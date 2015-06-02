4be9736878b19b1a8954f779dec68bd22721c300
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-02 17:54:33 -0400

    Adds install/uninstall script functionality

---
+++ bin/uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##
+# Uninstallation script for mbp-user-import
+##
+
+# Assume messagebroker-config repo is one directory up
+cd ..
+
+# Remove symlink to configuration settings repo
+rm messagebroker-config
+
