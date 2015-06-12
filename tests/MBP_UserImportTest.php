00941722ddf0500518e218bbbb7171ce0f34c76d
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2015-06-12 17:54:45 -0400

    Cleanup

--- tests/MBP_UserImportTest.php
+++ tests/MBP_UserImportTest.php
@@ -2,11 +2,11 @@
  
 use DoSomething\MBP_UserImport\MBP_UserImport;
 
-    // Including that file will also return the autoloader instance, so you can store
-    // the return value of the include call in a variable and add more namespaces.
-    // This can be useful for autoloading classes in a test suite, for example.
-    // https://getcomposer.org/doc/01-basic-usage.md
-    $loader = require_once __DIR__ . '/../vendor/autoload.php';
+// Including that file will also return the autoloader instance, so you can store
+// the return value of the include call in a variable and add more namespaces.
+// This can be useful for autoloading classes in a test suite, for example.
+// https://getcomposer.org/doc/01-basic-usage.md
+$loader = require_once __DIR__ . '/../vendor/autoload.php';
  
 class MBP_UserImportTest extends PHPUnit_Framework_TestCase {
   
