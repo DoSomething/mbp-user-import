cf143ba8c88daa606d064796da6bb63a5abfa7bb
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-01-15 18:57:47 -0500

    Refactored to source and targetFile collection as function

--- mbp-user-import.php
+++ mbp-user-import.php
@@ -9,49 +9,60 @@
  * triggered by each entry.
  */
 
+use DoSomething\MBP_UserImport\MBP_UserImport;
+
 date_default_timezone_set('America/New_York');
+define("ALLOWED_SOURCES", serialize([
+  'niche',
+  'afterSchool'
+]));
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBP_UserImport\MBP_UserImport;
-
 require_once __DIR__ . '/mbp-user-import.config.inc';
 
 echo '------- mbp-user-import START: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
-$targetFile = 'nextFile';
-if (isset($_GET['targetFile'])) {
-  $targetFile = $_GET['targetFile'];
-}
-elseif (isset($argv[1])) {
-  $targetFile = $argv[1];
-}
-
-$allowedSources = array(
-  'niche',
-);
-
-$source = NULL;
-if (isset($_GET['source'])) {
-  $source = $_GET['source'];
-}
-elseif (isset($argv[2])) {
-  $source = $argv[2];
-}
-
-if ($source != NULL)  {
-
-  if (in_array($source, $allowedSources)) {
-    $mbpUserImport = new MBP_UserImport($credentials, $config, $settings);
-    $mbpUserImport->produceCSVImport($targetFile, $source);
-  }
-  else {
-    echo 'ERROR - invalid source.', PHP_EOL;
-  }
-
+list($targetFile, $source) = gatherParameters();
+if (!empty($source))  {
+  $mbpUserImport = new MBP_UserImport();
+  $mbpUserImport->produceCSVImport($targetFile, $source);
 }
 else {
   echo '"source" parameter not defined.', PHP_EOL;
 }
 
 echo '------- mbp-user-import END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
+
+/**
+ * gatherParameters() - gather parameters set when starting application.
+ *
+ * @return
+ *   $targetFile string: the name of the file to process or "nextFile" (default).
+ *   $source string: one of the supported source (co-registration) values.
+ */
+function gatherParameters() {
+
+  $targetFile = 'nextFile';
+  if (isset($_GET['targetFile'])) {
+    $targetFile = $_GET['targetFile'];
+  }
+  elseif (isset($argv[1])) {
+    $targetFile = $argv[1];
+  }
+
+  $source = NULL;
+  if (isset($_GET['source'])) {
+    $source = $_GET['source'];
+  }
+  elseif (isset($argv[2])) {
+    $source = $argv[2];
+  }
+
+  $allowedSources = unserialize(ALLOWED_SOURCE);
+  if (in_array($source, $allowedSources)) {
+    die('Invalid source value. Acceptable values: ' . print_r($allowedSources, true));
+  }
+
+  return [$targetFile, $source];
+}
