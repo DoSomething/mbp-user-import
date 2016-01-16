59435826f1c8b01c2b8df4d3c440564606bee56a
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-01-15 19:04:08 -0500

    Refactored to move parameter gathering to function

--- mbp-user-import_manageData.php
+++ mbp-user-import_manageData.php
@@ -6,43 +6,52 @@
  * the file in data directory to be detercted by import script for processing.
  */
 
+use DoSomething\MBP_UserImport\MBP_UserCSVfileTools;
+
 date_default_timezone_set('America/New_York');
+define("ALLOWED_SOURCES", serialize([
+  'niche',
+  'afterSchool'
+]));
 
 // Load up the Composer autoload magic
 require_once __DIR__ . '/vendor/autoload.php';
-use DoSomething\MBP_UserImport\MBP_UserCSVfileTools;
-
 require_once __DIR__ . '/mbp-user-import_manageData.config.inc';
-
 
 echo '------- mbp-user-import_manageData START: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
 
 // Kick off
-$allowedSources = array(
-  'niche',
-);
-
-$source = NULL;
-if (isset($_GET['source'])) {
-  $source = $_GET['source'];
-}
-elseif (isset($argv[1])) {
-  $source = $argv[1];
-}
-
-if ($source != NULL)  {
-
-  if (in_array($source, $allowedSources)) {
+$source = gatherParameters();
+if (!empty($source))  {
     $mbpUserCSVfileTools = new MBP_UserCSVfileTools($settings);
     $status = $mbpUserCSVfileTools->gatherIMAP($source);
-  }
-  else {
-    echo 'ERROR - invalid source.', PHP_EOL;
-  }
-
 }
 else {
   echo '"source" parameter not defined.', PHP_EOL;
 }
 
 echo '------- mbp-user-import_manageData  END: ' . date('j D M Y G:i:s T') . ' -------', PHP_EOL;
+
+/**
+ * gatherParameters() - gather parameters set when starting application.
+ *
+ * @return
+ *   $source string: one of the supported source (co-registration) values.
+ */
+function gatherParameters() {
+
+  $source = NULL;
+  if (isset($_GET['source'])) {
+    $source = $_GET['source'];
+  }
+  elseif (isset($argv[1])) {
+    $source = $argv[1];
+  }
+
+  $allowedSources = unserialize(ALLOWED_SOURCE);
+  if (in_array($source, $allowedSources)) {
+    die('Invalid source value. Acceptable values: ' . print_r($allowedSources, true));
+  }
+
+  return $source;
+}
