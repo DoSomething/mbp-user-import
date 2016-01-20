6e30375e6140c348e5651479ddef52456d326a2f
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2016-01-20 15:59:53 -0500

    Properties and method values specific to niche as an import source

--- src/MBP_UserImport_Source_Niche.php
+++ src/MBP_UserImport_Source_Niche.php
@@ -15,6 +15,7 @@ use DoSomething\MB_Toolbox\MB_Configuration;
  */
 class MBP_UserImport_Source_Niche extends MBP_UserImport_BaseSource
 {
+
   /**
    * A list of supported keys in the CSV file provided by the source.
    *
@@ -34,14 +35,60 @@ class MBP_UserImport_Source_Niche extends MBP_UserImport_BaseSource
    * Supported key / columns in CSV file from source.
    */
   private function setKeys() {
-    
+
+    $keys = [
+      'first_name',
+      'last_name',
+      'email',
+      'address1',
+      'address2',
+      'city',
+      'state',
+      'zip',
+      'phone',
+      'hs_gradyear',
+      'birthdate',
+      'race',
+      'religion',
+      'hs_name',
+      'college_name',
+      'major_name',
+      'degree_type',
+      'sat_math',
+      'sat_verbal',
+      'sat_writing',
+      'act_math',
+      'act_english',
+      'gpa',
+      'role',
+    ];
+
+    return $keys;
   }
   
   /**
    * Logic to process CSV file based on column / line endings.
+   *
+   * @param array $CSVRow
+   *   A row of user data from the CSV file.
+   *
+   * @return array $data
+   *   
    */
-  public function process() {
-    
+  public function process($CSVRow) {
+
+    $CSVRow = str_replace('"', '',  $CSVRow);
+    $CSVRow = str_replace("\r\n", '',  $CSVRow);
+    $CSVData = explode(',', $CSVRow);
+
+    $data = array();
+    foreach ($this->keys as $signupIndex => $signupKey) {
+      if (isset($CSVData[$signupIndex]) && $CSVData[$signupIndex] != '') {
+        $data[$signupKey] = $signupData[$signupIndex];
+      }
+    }
+
+    return $data;
   }
 
 }
